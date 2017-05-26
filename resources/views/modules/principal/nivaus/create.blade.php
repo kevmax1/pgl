4de38d26ec08a3eb8451f841a6a70b4136d9cf3a
wamba zoko kévin <wambakevin7@gmail.com>
2017-05-26 13:16:45 +0100

    ajout crud niveau

--- resources/views/modules/principal/nivaus/create.blade.php
+++ resources/views/modules/principal/nivaus/create.blade.php
@@ -1,22 +1,35 @@
 @extends('layouts.app')
 
+@section('page-head')
+    <div class="page-head">
+        <h2 class="page-head-title">Niveaux</h2>
+        <ol class="breadcrumb page-head-nav">
+            <li><a href="#">Module principal</a></li>
+            <li><a href="#">niveaux</a></li>
+            <li class="active">Ajouter</li>
+        </ol>
+    </div>
+@endsection
+
 @section('content')
-    <section class="content-header">
-        <h1>
-            Nivau
-        </h1>
-    </section>
-    <div class="content">
-        @include('adminlte-templates::common.errors')
-        <div class="box box-primary">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="panel panel-default panel-border-color panel-border-color-primary">
+                <div class="panel-heading panel-heading-divider">Ajouter un niveau<span class="panel-subtitle"></span></div>
+                <div class="panel-body">
+                    @include('adminlte-templates::common.errors')
+                    <div class="box box-primary">
 
-            <div class="box-body">
-                <div class="row">
-                    {!! Form::open(['route' => 'nivaus.store']) !!}
+                        <div class="box-body">
+                            <div class="row">
+                                {!! Form::open(['route' => 'niveaux.store']) !!}
 
-                        @include('nivaus.fields')
+                                @include('modules.principal.nivaus.fields')
 
-                    {!! Form::close() !!}
+                                {!! Form::close() !!}
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
